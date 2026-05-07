f6121b3e622838d609ddb5f6a4c5b38be95fef25
Madhushivaganga KS <madhushivagangaks@gmail.com>
2026-05-07 18:46:34 +0530

    Updated

--- research_and_analyst/utils/config_loader.py
+++ research_and_analyst/utils/config_loader.py
@@ -10,7 +10,7 @@ def _project_root() -> Path:
     """
     Determine the absolute path of the project root directory.
     Example:
-        /Users/sunny/automated-research-report-generation/research_and_analyst
+        /Users/madhushivagangaks/automated-research-report-generation/research_and_analyst
     """
     return Path(__file__).resolve().parents[1]
 
